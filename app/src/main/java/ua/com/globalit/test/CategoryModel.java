ff6071c78cd95d1e3191a1e18253e91c4e4c290e
WoTi9l <navye100@gmail.com>
2016-07-04 23:46:04 +0300

    MoneyKeeper start

---
+++ app/src/main/java/ua/com/globalit/test/CategoryModel.java
@@ -0,0 +1,15 @@
+package ua.com.globalit.test;
+
+/**
+ * Created by user on 12/7/2015.
+ */
+public class CategoryModel {
+
+    public String Name;
+    public float Value;
+
+    public CategoryModel(String name, int value) {
+        Name = name;
+        Value = value;
+    }
+}
